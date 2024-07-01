72f3af91367e16d4f91473ec4089827299ab968d
Andrey Masunov <andrey.masunov@dci-student.org>
2024-07-01 03:12:04 +0200

    Implemented connectionFactory for DB connection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.example.config.ConnectionFactory;
+
 /**
  * Hello world!
  *
  */
 public class Main
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws SQLException {
+        Connection connection = ConnectionFactory.getConnection();
+        if ( connection != null ) {
+            System.out.println("DB connection established");
+        }
     }
 }
--- src/main/java/org/example/config/ConnectionFactory.java
+++ src/main/java/org/example/config/ConnectionFactory.java
@@ -1,5 +1,27 @@
 package org.example.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
 
+  private static final String URL = "jdbc:postgresql://localhost:5432/carbon_emission";
+  private static final String USER = "postgres";
+  private static final String PASSWORD = "111111";
+  private static final String POSTGRES_DRIVER = "org.postgresql.Driver";
+
+  private ConnectionFactory(){
+  }
+
+  public static Connection getConnection() throws SQLException {
+    try {
+      Class.forName(POSTGRES_DRIVER);
+      Connection connection = null;
+      connection = DriverManager.getConnection(URL, USER, PASSWORD);
+      return connection;
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
